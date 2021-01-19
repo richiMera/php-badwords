d85313c498939d2edff8e5704091bacd0c144f08
Riccardo Ferrari <richi.f93@gmail.com>
2021-01-19 16:13:40 +0100

    correction, variable in echo

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 $paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 $badWord = $_GET["word"];
 
@@ -11,7 +11,7 @@ $badWord = $_GET["word"];
     <title></title>
   </head>
   <body>
-    <h1><?php echo str_replace($_GET["word"], "***", $paragraph); ?></h1>
+    <h1><?php echo str_replace($badWord, "***", $paragraph); ?></h1>
     <h3>La lunghezza del paragrafo è: <?php echo strlen($paragraph); ?> </h3>
   </body>
 </html>
