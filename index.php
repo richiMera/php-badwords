ca010d49fcd492083137a5925a812fbd80c0b876
Riccardo Ferrari <richi.f93@gmail.com>
2021-01-19 16:00:02 +0100

    done

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+$paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+$badWord = $_GET["word"];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1><?php echo str_replace($_GET["word"], "***", $paragraph); ?></h1>
+    <h3>La lunghezza del paragrafo è: <?php echo strlen($paragraph); ?> </h3>
+  </body>
+</html>
